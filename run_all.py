17419499e3574932420fc854db86299a1dc07e3d
WebisD <weverson.webis@gmail.com>
2022-10-15 18:37:58 -0300

    Run all tests file

--- run_all.py
+++ run_all.py
@@ -3,38 +3,51 @@ import time
 import numpy as np
 import dicomHandler as dicomHandler
 import wspMultithreshold as wspMultithreshold
+import concurrent.futures
 
 input_dir = "results/2EFVJVCN"
+
+all_slices = os.listdir(input_dir)
 
 bio_algorithms = ['FFA', 'KH', 'CS', 'ABC', 'EHO']
 q_values = np.linspace(-2, 2, num=41)
 dimension = [1,2,3,4,5]
 
-def segmentate(image_folder, algorithm, dimension, q):
+output_dir_high = ""
+output_dir_big = ""
+current_bio = ""
+current_dim = 0
+current_q = 0
 
-    output_dir_big = input_dir + f"-BigReg-{algorithm}-d{dimension}-q{q}"
-    output_dir_high = input_dir + f"-HighIntensity-{algorithm}-d{dimension}-q{q}"
+def segment_slice(dcm_file_path):
+    dicom_img = input_dir + '/' + dcm_file_path
     
-    os.mkdir(output_dir_big)
-    os.mkdir(output_dir_high)
+    dicom_image, pixel_array = dicomHandler.read_dicom_image(dicom_img)
 
-    for file in os.listdir(image_folder):
-        dcm_path = image_folder + '/' + file
+    hu_image = dicomHandler.transform_to_hu(dicom_image, pixel_array)
 
-        dicom_image, pixel_array = dicomHandler.read_dicom_image(dcm_path)
+    hist, bin_edges, best_thresholds, img_thres = wspMultithreshold.wspMultithreshold(hu_image, current_bio, current_dim, current_q)
 
-        hu_image = dicomHandler.transform_to_hu(dicom_image, pixel_array)
+    high_intensity = wspMultithreshold.get_high_intensity_pixels(img_thres)
 
-        hist, bin_edges, best_thresholds, img_thres = wspMultithreshold.wspMultithreshold(hu_image, algorithm, dimension, q)
+    pixel_array = dicomHandler.transform_to_pixel_array(dicom_image, high_intensity)
 
-        high_intensity = wspMultithreshold.get_high_intensity_pixels(img_thres)
+    hard_tissue = wspMultithreshold.get_largest_region(pixel_array)
 
-        pixel_array = dicomHandler.transform_to_pixel_array(
-            dicom_image, high_intensity)
+    dicomHandler.save_dicom(dicom_image, pixel_array, f'{output_dir_high}/{dcm_file_path}')
+    dicomHandler.save_dicom(dicom_image, hard_tissue, f'{output_dir_big}/{dcm_file_path}')
 
-        hard_tissue = wspMultithreshold.get_largest_region(pixel_array)
 
-        dicomHandler.save_dicom(dicom_image, pixel_array, f'{output_dir_high}/{file}')
-        dicomHandler.save_dicom(dicom_image, hard_tissue, f'{output_dir_high}/{file}')
+for dim in dimension:
+    for bio in bio_algorithms:
+        for q_v in q_values:
+            current_bio = bio
+            current_dim = dim
+            current_q = q_v
+            output_dir_big = input_dir + f"-BigReg-{bio}-d{dim}-q{q_v}"
+            output_dir_high = input_dir + f"-HighIntensity-{bio}-d{dim}-q{q_v}" 
+            os.mkdir(output_dir_big)
+            os.mkdir(output_dir_high)
 
-    return
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                executor.map(segment_slice, all_slices)
